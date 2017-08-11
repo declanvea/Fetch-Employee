56b5d65f1f955950025b056a6ec74958d740086d
Declan Vea <declanpvea@gmail.com>
2017-08-10 21:41:02 -0400

    fixed and final

--- main.js
+++ main.js
@@ -1,98 +1,34 @@
 
-let box = document.getElementsByTagName("td");
-let url = "http://jsonplaceholder.typicode.com/users";
-let search = document.getElementById("search");
+let apiURL = "http://jsonplaceholder.typicode.com/users?=";
+let select = document.querySelector('select');
+let button = document.querySelector('button');
+let container = document.querySelector('.container');
 
-fetch(url)
-.then(function(response){
-  return response.json(buttonClick);
-})
-.then(function(data){
-  console.log(data);
-})
+button.addEventListener("click", function(){
+    fetch(apiURL).then(function(response) {
+        response.json().then(dataOptions);
+      }
+    )
+});
 
-function buttonClick() {
-  for (let i = 0; i < data.length; i++) {
-    if (search === data[i].id){
-      let url = `http://jsonplaceholder.typicode.com/${search}`;
-
-      let name = document.createElement("p");
-      name.innerHTML = `Name: ${data[i].name}`;
-      box.appendChild(name);
-
-      let email = document.querySelector("p");
-      email.innerHTML = `Email: ${data[i].email}`;
-      box.appendChild(email);
-
-      let phone = document.createElement("tel");
-      phone.innerHTML = `Phone Number: ${data[i].phone}`;
-      box.appendChild(phone);
-    } else {
-      alert("That is not the name of an employee, please try again.")
+function dataOptions(data) {
+  for (var i = 0; i < data.length; i++) {
+// By setting the id equal to the select option value, I can sort by id === the index and it will be the same thing as selecting the id
+    if (data[i].id === select.selectedIndex) {
+// created div(s) element for data to populate in when pulled
+      let div = document.createElement('div');
+// defined needed variables
+      let id = data[i].id;
+      let name = data[i].name;
+      let email = data[i].email;
+      let phone = data[i].phone;
+// innerHTML allows me to print the fields in text that I want, inside the div(s)
+      div.innerHTML =`<p>Employee ID: ${id}</p>
+                      <p>Name: ${name}</p>
+                      <p>Email: ${email}</p>
+                      <p>Phone: ${phone}</p>`;
+// appending the created div(s) to the container section
+      container.appendChild(div);
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-// function employee(data){
-//   // for (let i = 0; i < data.length; i++) {
-//     // if (search.data[i].id < data.length){
-//       let name = document.createElement("p");
-//       name.innerHTML = `Name: ${data.name}`;
-//       box.appendChild(name);
-//
-//       let email = document.querySelector("p");
-//       email.innerHTML = `Email: ${data.email}`;
-//       box.appendChild(email);
-//
-//       let phone = document.createElement("tel");
-//       phone.innerHTML = `Phone Number: ${data.phone}`;
-//       box.appendChild(phone);
-//     // }
-//   // }
-// }
-
-
-
-
-
-
-
-
-
-// document.getElementById("search")
-// .addEventListener("keyup", function(event) {
-// event.preventDefault();
-// if (event.keyCode === 13) {
-//   document.getElementById("search").click();
-// }
-// });
-// ______________________________________________________________________
-
-// function recipePuppy(){
-// console.log("Posting request to Recipe Puppy API...");
-// fetch("http://www.recipepuppy.com/api/?q=omelet", {
-//   method: "post",
-//   mode: 'no-cors'
-// })
-//   .then(function(response){
-//     return response.json();
-//   })
-//   .catch(function(err) {
-//     console.log("Fetch Error :-S", err);
-//   });
-// }
-
-// const options = {
-//   method: "post",
-//   mode: 'no-cors'
-// }
